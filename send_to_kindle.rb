979df8c2db6e5b60a0ae3608d14330801062037c
Jonathan Senior <jonosenior@gmail.com>
2020-12-28 10:46:33 +0000

    Scrape individual articles and save to local files (with random filenames)

--- send_to_kindle.rb
+++ send_to_kindle.rb
@@ -5,7 +5,8 @@ require 'pony'
 require 'dotenv/load'
 
 def fetch_article_links(articles_index_url)
-  articles_index_page = Nokogiri::HTML(URI.open(articles_index_url))
+  articles_uri = URI.open(articles_index_url)
+  articles_index_page = Nokogiri::HTML(articles_uri)
   all_links = articles_index_page.css('div article a').map{ |link| link['href'] }
 
   external_article_links = all_links.select do |url|
@@ -13,10 +14,14 @@ def fetch_article_links(articles_index_url)
   end.uniq
 end
 
-def email_html_to_kindle(html)
+def fetch_article_html(url)
+  Nokogiri::HTML(URI.open(url))
+end
+
+def send_to_kindle(filenames)
   Pony.mail :to => ENV["target_email_address"],
             :subject => "Here are your articles",
-            :body =>  html,
+            :body => '',
             :via => :smtp,
             :via_options => {
                 :address              => 'smtp.gmail.com',
@@ -28,3 +33,17 @@ def email_html_to_kindle(html)
             },
             :attachments          => { "example.html" => File.read("example.html"), "example2.html" => File.read("example2.html"), "example3.html" => File.read("example3.html"), "example4.html" => File.read("example4.html") }
 end
+
+def random_string
+  (0...8).map { (65 + rand(26)).chr }.join
+end
+
+def save_html_files(links)
+  links.each do |link|
+    html = fetch_article_html(link)
+    File.open("#{random_string}.html", "w") {|file| file.write(html) }
+  end
+end
+
+links = fetch_article_links('https://longform.org/best')
+save_html_files(links)
